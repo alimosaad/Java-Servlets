d4284071c0b11b05560dfdde3e191bacba4cb91b
ali mosaad <alimosaad573@gmail.com>
2025-05-28 06:51:05 +0300

    Begin with servlets.

---
+++ src/main/java/org/example/demo/addTest.java
@@ -0,0 +1,24 @@
+package org.example.demo;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/addTest"})
+public class addTest extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        int i =Integer.parseInt(req.getParameter("num1"));
+        int i2 =Integer.parseInt(req.getParameter("num2"));
+         int k=i+i2;
+        PrintWriter out = res.getWriter();
+        out.println(k);
+    }
+
+}
